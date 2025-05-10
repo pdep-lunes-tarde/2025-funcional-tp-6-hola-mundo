2baff79614ddde74f84e56a4418e668c2d320160
TomasAArin <tarin@frba.utn.edu.ar>
2025-05-10 03:01:46 -0300

    Parte 1 casi finalizada

--- src/Library.hs
+++ src/Library.hs
@@ -5,6 +5,7 @@ data Ingrediente =
     Carne | Pan | Panceta | Cheddar | Pollo | Curry | QuesoDeAlmendras
     deriving (Eq, Show)
 
+precioIngrediente :: Ingrediente -> Number
 precioIngrediente Carne = 20
 precioIngrediente Pan = 2
 precioIngrediente Panceta = 10
@@ -17,4 +18,32 @@ data Hamburguesa = Hamburguesa {
     precioBase :: Number,
     ingredientes :: [Ingrediente]
 } deriving (Eq, Show)
+
+
+precioFinal :: Hamburguesa -> Number
+precioFinal h = precioBase h + sum( map precioIngrediente (ingredientes h ) )
+
+
+elegirBase :: [Ingrediente] -> [Ingrediente]
+elegirBase cola
+  | any (== Carne) cola = [Carne]
+  | any (==Pollo) cola = [Pollo]
+  | otherwise = []
+
+cuartoDeLibra :: Hamburguesa
+cuartoDeLibra = Hamburguesa 20 [ Pan, Carne, Cheddar, Pan]
+
+agrandar :: Hamburguesa -> Hamburguesa
+agrandar h = Hamburguesa (precioBase h) (ingredientes h ++ elegirBase (ingredientes h ))
+
+agregarIngrediente :: Ingrediente -> Hamburguesa -> Hamburguesa
+agregarIngrediente ingredie h = Hamburguesa (precioBase h) (ingredientes h ++ [ingredie])
+
+descuento :: Number -> Hamburguesa -> Hamburguesa
+descuento porciento h = Hamburguesa (precioBase h * (1-porciento / 100) ) (ingredientes h)
+
+pdepBurger :: Hamburguesa
+
+
+
 
